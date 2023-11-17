203642bfa8dcf4029dcdd63a108c780499112e38
EC2 Default User <ec2-user@ip-172-31-31-12.eu-west-1.compute.internal>
2023-11-17 14:53:40 +0000

    changes med instruksjoner og laget bucket kandidat2028

--- kjell/hello_world/app.py
+++ kjell/hello_world/app.py
@@ -16,9 +16,30 @@ s3_client = boto3.client('s3', region_name='eu-west-1')
 rekognition_client = boto3.client('rekognition', region_name='eu-west-1')
 
 # Oppgave 1A
-BUCKET_NAME = "kjellsimagebucker"
+BUCKET_NAME = os.environ.get('BUCKET_NAME', 'default_bucket_name') 
+#her kan du bytte "default_bucket_name" med kandidat2028 for å se bucketen jeg har laget
+# export BUCKET_NAME=ENNYTEST
+# echo $BUCKET_NAME
+# Disse her lager en miljøvariabel for selve bucket navnet, echo er for å sjekke om du har laget  variabelen
+# neste steger er å cd seg selv helt ned til hello world mappen og skrive "python3 app.py" da blir en bucket skrevet og laget med variabel navnet
+    
+
+
+def create_s3_bucket(bucket_name):
+    try:
+        location_constraint = s3_client.meta.region_name  
+        s3_client.create_bucket(
+            Bucket=bucket_name,
+            CreateBucketConfiguration={'LocationConstraint': location_constraint}
+        )
+        print(f"The Bucket '{bucket_name}' was created")
+    except s3_client.exceptions.BucketAlreadyOwnedByYou as e:
+        print(f"The bucket '{bucket_name}' is already made.")
+    except Exception as e:
+        print(f"The bucket was not created because of an error '{bucket_name}': {str(e)}")
 
 def lambda_handler(event, context):
+    create_s3_bucket(BUCKET_NAME)
 
     # List all objects in the S3 bucket
     paginator = s3_client.get_paginator('list_objects_v2')
