308aefcdbb19a3a4853796f912e39d9da67c4b6a
EC2 Default User <ec2-user@ip-172-31-31-12.eu-west-1.compute.internal>
2023-11-26 15:37:20 +0000

    new changes

--- kjell/hello_world/app.py
+++ kjell/hello_world/app.py
@@ -9,24 +9,6 @@ rekognition_client = boto3.client('rekognition', region_name='eu-west-1')
 BUCKET_NAME = os.environ.get ('BUCKET_NAME')
 print(f"Retrieved bucket name: {BUCKET_NAME}")  # Debug print
 
-
-def create_s3_bucket(bucket_name):
-    try:
-        location_constraint = s3_client.meta.region_name
-        s3_client.create_bucket(
-            Bucket=bucket_name,
-            CreateBucketConfiguration={'LocationConstraint': location_constraint}
-        )
-        print(f"The Bucket '{bucket_name}' was created")
-    except s3_client.exceptions.BucketAlreadyOwnedByYou as e:
-        print(f"The bucket '{bucket_name}' is already made.")
-    except Exception as e:
-        print(f"The bucket was not created because of an error '{bucket_name}': {str(e)}")
-
-def lambda_handler(event, context):
-    create_s3_bucket(BUCKET_NAME)
-
-    # List all objects in the S3 bucket
     paginator = s3_client.get_paginator('list_objects_v2')
     rekognition_results = []  # Store the results
 
