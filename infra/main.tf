5c60aabea89431615ff8c40aea5eab16f45c5013
EC2 Default User <ec2-user@ip-172-31-31-12.eu-west-1.compute.internal>
2023-11-27 19:02:04 +0000

    new changes

--- infra/main.tf
+++ infra/main.tf
@@ -4,7 +4,7 @@ resource "aws_apprunner_service" "service" {
   instance_configuration {
     instance_role_arn = aws_iam_role.role_for_apprunner_service.arn
   }
-
+#test
   source_configuration {
     authentication_configuration {
       access_role_arn = "arn:aws:iam::244530008913:role/service-role/AppRunnerECRAccessRole"
