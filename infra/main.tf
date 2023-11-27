a0ded809683747c7d7f04bda1344960373aac7d1
EC2 Default User <ec2-user@ip-172-31-31-12.eu-west-1.compute.internal>
2023-11-27 17:34:09 +0000

    maybe last changes who knows hhehehe

--- infra/main.tf
+++ infra/main.tf
@@ -70,11 +70,3 @@ resource "aws_apprunner_service" "service" {
     auto_deployments_enabled = true
   }
 }
-
-terraform {
-  backend "s3" {
-    bucket = "kandidat-2028"
-    key    = "terraform.tfstate"
-    region = "eu-west-1"
-  }
-}
