e72d5caa42086a52c1e665b0063584af7f4de072
EC2 Default User <ec2-user@ip-172-31-31-12.eu-west-1.compute.internal>
2023-11-27 17:37:27 +0000

    adjusting the file

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,5 @@
-terraform {
-  backend "s3" {
-    bucket = "kandidat-2028"
-    key    = "terraform.tfstate"
-    region = "eu-west-1"
-  }
+backend "s3" {
+  bucket = "kandidat-2028"
+  key    = "terraform.tfstate"
+  region = "eu-west-1"
 }
